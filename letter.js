8f148a82e32a2c472a7e6d31512ae89c3b6d9a5e
Cragady <pinkdotdreamer@gmail.com>
2018-07-01 21:43:02 -0600

    fixed letters constructor

--- letter.js
+++ letter.js
@@ -1,13 +1,63 @@
 //this does NOT require any other files
-function Letter(word){
-    this.letterAct =[];
-    this.word = word;
-    var lets = word.split("");
-    for(i = 0; i < lets.length; i++){
-        this.letterAct.push({letter: lets[i], letterGuessed: false});
+function Letter(letIn){
+    // this.letterAct =[];
+    // var lets = word.split("");
+    // for(i = 0; i < lets.length; i++){
+    //     this.letterAct.push({
+    this.letter = {
+        letter: letIn, 
+        letterGuessed: false,
+        letterChecker: function(letterGuessIn){
+            this.writeWord(letterGuessIn);
+            if(this.letterGuessed === true){
+                this.swapper = this.letter;
+            } else {
+                this.swapper =  "_";
+            }
+            console.log(this.swapper);
+        },
+        writeWord: function(letterGuessIn){
+            if(this.letter === letterGuessIn){
+                this.letterGuessed = true;
+            }
+        }
     }
+    //     });
+    // }
+
+    // this.letterChecker = function(word, letter){
+    //     var wordShow = "";
+    //     for(i = 0; i < word.word.length; i++){
+    //         if(word.letterAct[i].letterGuessed === true){
+    //             wordShow += word.letterAct[i].letter;
+    //         } else {
+    //             wordShow += "_";
+    //         };
+    //     }
+    //     this.writeWord(word, letter);
+    //     console.log(wordShow);
+    // }
+    // this.writeWord = function(word, letter){
+    //     for(i = 0; i < word.word.length; i++){
+    //         if(word.letterAct[i].letter === letter){
+    //             word.letterAct[i].letterGuessed = true;
+    //             console.log(word);
+    //         }
+    //     }
+    // }
 };
+module.exports = Letter;
 
-var horse = new Letter("horse");
+hoover = new Letter("h");
 
-console.log(horse);
+guessedSoFar = "h";
+hoover.letter.letterChecker(guessedSoFar);
+// letterLooper = function(inputLet, letterReturn){
+//     returner = "";
+    
+//     return guessedSoFar = letterReturn;
+// }
+
+// letterLooper("o", guessedSoFar);
+// letterLooper("h");
+// letterLooper("z");
--- word.js
+++ word.js
@@ -1,1 +1,20 @@
 //should ONLY require letters.js
+var Letter = require("./letter");
+var choicedWord = "hooover";
+var activeWord;
+
+function Word(word){
+    this.word = word;
+    wordPass = new Letter(word);
+    this.letterAct = wordPass.letterAct;
+    this.letterChecker = wordPass.letterChecker;
+    this.writeWord = wordPass.writeWord;    
+};
+
+newFunk = function(getIt){
+    activeWord = new Word(getIt);
+}
+// activeWord.letterChecker(activeWord.word, "o");
+newFunk(choicedWord);
+console.log(activeWord.letterAct);
+
