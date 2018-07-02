b05f5d8edcdab5932b7c91f145b12b41ac09e6bb
Cragady <pinkdotdreamer@gmail.com>
2018-07-01 22:57:07 -0600

    cleaned up letter.js and fixed an issue; finishing up word.js

--- letter.js
+++ letter.js
@@ -1,9 +1,4 @@
-//this does NOT require any other files
 function Letter(letIn){
-    // this.letterAct =[];
-    // var lets = word.split("");
-    // for(i = 0; i < lets.length; i++){
-    //     this.letterAct.push({
     this.letter = {
         letter: letIn, 
         letterGuessed: false,
@@ -14,50 +9,12 @@ function Letter(letIn){
             } else {
                 this.swapper =  "_";
             }
-            console.log(this.swapper);
         },
         writeWord: function(letterGuessIn){
-            if(this.letter === letterGuessIn){
+            if(this.letter === letterGuessIn || this.letter === " "){
                 this.letterGuessed = true;
             }
         }
     }
-    //     });
-    // }
-
-    // this.letterChecker = function(word, letter){
-    //     var wordShow = "";
-    //     for(i = 0; i < word.word.length; i++){
-    //         if(word.letterAct[i].letterGuessed === true){
-    //             wordShow += word.letterAct[i].letter;
-    //         } else {
-    //             wordShow += "_";
-    //         };
-    //     }
-    //     this.writeWord(word, letter);
-    //     console.log(wordShow);
-    // }
-    // this.writeWord = function(word, letter){
-    //     for(i = 0; i < word.word.length; i++){
-    //         if(word.letterAct[i].letter === letter){
-    //             word.letterAct[i].letterGuessed = true;
-    //             console.log(word);
-    //         }
-    //     }
-    // }
 };
 module.exports = Letter;
-
-hoover = new Letter("h");
-
-guessedSoFar = "h";
-hoover.letter.letterChecker(guessedSoFar);
-// letterLooper = function(inputLet, letterReturn){
-//     returner = "";
-    
-//     return guessedSoFar = letterReturn;
-// }
-
-// letterLooper("o", guessedSoFar);
-// letterLooper("h");
-// letterLooper("z");
--- word.js
+++ word.js
@@ -1,20 +1,46 @@
 //should ONLY require letters.js
 var Letter = require("./letter");
-var choicedWord = "hooover";
-var activeWord;
+var choicedWord = "hooover damn";
 
 function Word(word){
     this.word = word;
-    wordPass = new Letter(word);
-    this.letterAct = wordPass.letterAct;
-    this.letterChecker = wordPass.letterChecker;
-    this.writeWord = wordPass.writeWord;    
+    this.letterAct = [];
+    this.lettersSpray = function(spreader){
+        lets = spreader.split("");
+        var wordShower = "";
+        for(i = 0; i < lets.length; i++){
+            letterPass = new Letter(lets[i]);
+            letterPass.letter.letterChecker(" ");
+            this.letterAct.push(letterPass);
+            wordShower += letterPass.letter.swapper;
+        }
+        console.log(wordShower);
+    };
+    this.charChecker = function(guessered){
+        var wordShower = "";
+        for(i = 0; i < this.letterAct.length; i++){
+            this.letterAct[i].letter.letterChecker(guessered);
+            wordShower += this.letterAct[i].letter.swapper;
+        }
+        console.log(wordShower);
+    } 
 };
 
 newFunk = function(getIt){
     activeWord = new Word(getIt);
+    activeWord.lettersSpray(getIt);
+    console.log(activeWord);
+    activeWord.charChecker("o");
+    activeWord.charChecker('h');
+    activeWord.charChecker('v');
+    activeWord.charChecker('e');
+    activeWord.charChecker('r');
+    activeWord.charChecker('d');
+    activeWord.charChecker('a');
+    activeWord.charChecker('m');
+    activeWord.charChecker('n');
+    
 }
 // activeWord.letterChecker(activeWord.word, "o");
 newFunk(choicedWord);
-console.log(activeWord.letterAct);
 
