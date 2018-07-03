d489e5f049ff507d53ba297d97930d3512102205
Cragady <pinkdotdreamer@gmail.com>
2018-07-02 21:03:44 -0600

    fully implemented words tracked, need to clean up code a little

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Word = require("./word");
 var inquirer = require("inquirer");
 var Choiced = require("./word-list");
 var choicedArray = Choiced.choices;
-var wordsGuessed = [];
+var wordsGuessed = [1, 2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49];
 var letterGuessed = [];
 var guessLeft = 10;
 
@@ -53,8 +53,9 @@ repeater = function(wordHere){
                 console.log("\x1b[32m CORRECT!! Congratulations!")
                 letterGuessed.push(answer.lettered);
                 letterWriter();
-                console.log("\x1b[33m Letters Guessed: " + letterGuessed);
-                repeater(wordHere);
+                writeTheLetters = letterGuessed.join(", ");
+                console.log("\x1b[33m Letters Guessed: " + writeTheLetters);
+                wordFinishChecker(wordHere);
                 return;
             } else if(i === wordHere.letterAct.length - 1 && guessLeft > 1){
                 guessLeft--;
@@ -62,7 +63,8 @@ repeater = function(wordHere){
 ${guessLeft} guess(es) remaining`);
                 letterGuessed.push(answer.lettered);
                 letterWriter();
-                console.log("\x1b[33m Letters Guessed: " + letterGuessed);
+                writeTheLetters = letterGuessed.join(", ");
+                console.log("\x1b[33m Letters Guessed: " + writeTheLetters);
                 repeater(wordHere);
             } else if (guessLeft === 1){
                 console.log("Oops");
@@ -71,13 +73,38 @@ ${guessLeft} guess(es) remaining`);
             }
         }
     });
-}
+};
+
+wordFinishChecker = function(objPass){
+    for(i = 0; i < objPass.letterAct.length; i++){
+        if(objPass.letterAct[i].letter.letterGuessed === false){
+            repeater(objPass);
+            return;
+        }
+    }
+    wordsGuessed.push(objPass.word);
+    if(wordsGuessed.length === choicedArray.length){
+        console.log(`\x1b[32m Wow. . . You're pretty much a god at this huh?
+Or you played this way too much. . .
+(Or you cheated. . . kidding!)        
+Anywho, CONGRATULATIONS! YOU GUESSED ALL THE WORDS I HAVE!! CRAZY!
+I don't have any special rewards, but you can play again if you like!`);
+        gameContinue();
+        return;
+    }
+    console.log("This was hit");
+    var wordWriter = wordsGuessed.join(", ");
+    console.log(`\x1b[32m YES!! Word has been guessed! Congratulations!!
+Words guessed so far: ${wordWriter}
+Guesses remaining: ${guessLeft}, good luck!`);
+    gameStarter();
+};
 
 gameContinue = function(){
     inquirer.prompt([
         {
             name: "continue",
-            message: "Continue?",
+            message: "Play again?",
             type: "confirm"
         }
     ]).then(answer =>{
